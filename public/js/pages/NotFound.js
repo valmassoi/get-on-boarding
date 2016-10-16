545e64f662a2fd89795143c93d0831948594bcb1
valmassoi <rvalmassoi@gmail.com>
2016-10-16 13:09:06 -0700

    style 404 page

--- public/js/pages/NotFound.js
+++ public/js/pages/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const NotFound = () =>
-  <div class="well well-lg">
+  <div class="well well-lg centered-both" style={{ width: '500px' }}>
     <h3>404: page not found</h3>
     <p>We are sorry but the page you are looking for does not exist.</p>
   </div>
