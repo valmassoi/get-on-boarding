c64fa93fbfc88f3654c1e9a37848d7408e490a77
valmassoi <rvalmassoi@gmail.com>
2016-10-16 13:08:54 -0700

    init save status in completion bar

--- public/js/components/CompletionBar.js
+++ public/js/components/CompletionBar.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 class CompletionBar extends Component {
 
   render() {
     const { stepNumber } = this.props
+    const saveStatus = '...saving TODO'// HACK
+    const tooltip = (
+      <Tooltip id="tooltip">
+        Your data has been saved. Come back anytime to complete the form.
+      </Tooltip>
+    )
     return (
       <div class="well centered" style={{ marginTop: '15px', width: '600px', backgroundColor: 'white' }}>
         <div class="progress">
           <div class="progress-bar progress-bar-success" style={{ width: (stepNumber - 1) * 100 / 6 + '%' }} />
         </div>
         Step {stepNumber} of 6
+        <span class="pull-right">
+          <OverlayTrigger placement="left" delayHide={500} overlay={tooltip}>
+            <p style={{ cursor: 'pointer' }} class="glyphicon glyphicon-info-sign" />
+          </OverlayTrigger>
+          &nbsp;{saveStatus}
+        </span>
       </div>
     )
   }
