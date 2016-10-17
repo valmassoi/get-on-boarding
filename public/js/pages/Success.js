9ab2a1cc0318beedc50f0283e8e45957dcf87268
valmassoi <rvalmassoi@gmail.com>
2016-10-16 17:37:19 -0700

    init success page

---
+++ public/js/pages/Success.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Success = () =>
+  <div class="well well-lg centered-both" style={{ width: '500px' }}>
+    <h3>Success</h3>
+    <p>Check your email to verify.</p>
+  </div>
+
+export default Success
