5681ce070ef3a4df76854fe9bc8c085c4b0f429c
valmassoi <rvalmassoi@gmail.com>
2016-11-05 14:12:32 -0600

    moving from mac -> linux

--- public/js/components/Form.js
+++ public/js/components/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { browserHistory } from 'react-router'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
+import { reduxForm } from 'redux-form'
 import * as formActions from '../actions/form'
 
 import Form1 from './Forms/Form1'
@@ -14,8 +15,8 @@ class Form extends Component {
     }
   }
 
-  onSubmit(e) {
-    e.preventDefault()
+  onSubmit(formData) {
+    console.log(formData);
     const step = this.props.stepNumber + 1
     if (true && step < 7) {
       this.props.setStep(step)
@@ -38,14 +39,31 @@ class Form extends Component {
     this.setState({ isBadInput: 'bad' })
     setTimeout(() => this.setState({ isBadInput: '' }), 1000)// HACK
   }
+  renderAlert() {
+   if (this.props.errorMessage) {
+     this.badAnimation()
+     return (
+       <div className="alert alert-danger">
+         <strong>Oops!</strong> {this.props.errorMessage}
+       </div>
+     )
+   }
+  }
 
   render() {
     const { isBadInput } = this.state
-    const { stepNumber } = this.props
-    const forms = [<Form1 />, <Form2 />, `FORM ${stepNumber}`, `FORM ${stepNumber}`, `FORM ${stepNumber}`, `FORM ${stepNumber}`]
+    const { stepNumber, handleSubmit, fields } = this.props
+    const forms = [
+      <Form1 fields={fields} />,
+      <Form2 />,
+      `FORM ${stepNumber}`,
+      `FORM ${stepNumber}`,
+      `FORM ${stepNumber}`,
+      `FORM ${stepNumber}`,
+    ]
     return (
       <div class={`form-container centered ${isBadInput}`}>
-        <form onSubmit={(e) => this.onSubmit(e)}>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <div class="card-header">Title of Form {stepNumber}</div>
             {forms[stepNumber-1]}
           <div class="btn btn-default pull-left" onClick={() => this.goBack()}>Back</div>
@@ -55,4 +73,12 @@ class Form extends Component {
     )
   }
 }
-export default connect(null, formActions)(Form)
+
+function mapStateToProps(state) {
+  return { errorMessage: '' } //state.form.error
+}
+//http://redux-form.com/5.3.3/#/examples/multirecord?_k=kcy5ns
+export default reduxForm({
+  form: 'this.props.stepNumber',
+  fields: ['version', 'build', 'note', 'user'],
+}, mapStateToProps, formActions)(Form)
--- public/js/components/Forms/Form1.js
+++ public/js/components/Forms/Form1.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Form1 = () => {
+const Form1 = ({ version, build, note, user } = fields) => {
   return (
     <div>
 <fieldset class="form-group" style={{ width: '300px', float: 'left' }}>
  <label>Version</label>
- <input class="form-control" placeholder="v2.2.2" />
+ <input {...version} class="form-control" placeholder="v2.2.2" />
 </fieldset>
 <fieldset class="form-group" style={{ width: '300px', float: 'left' }}>
  <label>Build</label>
- <input class="form-control" placeholder="282" />
+ <input {...build} class="form-control" placeholder="282" />
 </fieldset>
 <fieldset class="form-group">
  <label>Production</label>
@@ -38,12 +38,12 @@ const Form1 = () => {
 </fieldset>
 <fieldset class="form-group">
  <label>Note</label>
- <input class="form-control" placeholder="Test some new function to see if it works" />
+ <input {...note} class="form-control" placeholder="Test some new function to see if it works" />
 </fieldset>
 <fieldset class="form-group">
  <label>User access</label>
  <br />
- <select>
+ <select {...user}>
    <option>asdffdas</option>
    <option>asfd</option>
    <option>asdfasdfasdfasd</option>
