8d073702336bc3133bad1693853b76e074e80ad2
valmassoi <rvalmassoi@gmail.com>
2016-11-06 09:31:01 -0700

    init redux persist

--- public/js/index.js
+++ public/js/index.js
@@ -5,15 +5,16 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { Router, browserHistory } from 'react-router'
 import reduxThunk from 'redux-thunk'
+import { persistStore, autoRehydrate } from 'redux-persist'
 
 import reducers from './reducers/index'
 import routes from './routes'
 
-//TODO http://stackoverflow.com/questions/35305661/where-to-write-to-localstorage-in-a-redux-app
+// TODO http://stackoverflow.com/questions/35305661/where-to-write-to-localstorage-in-a-redux-app
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
-const store = createStoreWithMiddleware(reducers)
-
+const store = createStoreWithMiddleware(reducers, undefined, autoRehydrate())
+persistStore(store)
 const app = document.getElementById('app')
 
 
